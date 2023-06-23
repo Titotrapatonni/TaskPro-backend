699a00cd4271e26a802f9e6b20a268352e3d4e04
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-06-23 16:32:09 +0300

    Added email sending logic

---
+++ controllers/sendEmailController.js
@@ -0,0 +1,33 @@
+const { controllerWrapper } = require("../decorators");
+const { sendEmail } = require("../helpers");
+
+const { SUPPORT_EMAIL } = process.env;
+
+const sendEmailMessage = async (req, res, next) => {
+  const { replyEmail, comment } = req.body;
+  const { email: userEmail } = req.user;
+  console.log(userEmail, replyEmail, comment);
+
+  const verifyEmail = {
+    to: SUPPORT_EMAIL,
+    subject: `From ${userEmail}`,
+    html: `
+            <div>
+                <h1 style="color: red; text-align: center;">${userEmail} need help!</h1>
+                <p style="font-size: 16px;">${comment}</p>
+                <hr style="border-color: blue;">
+                <p style="font-style: italic;">reply to address ${replyEmail} please</p>
+            </div>
+        `,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.status(200).json({
+    message: "Email sent",
+  });
+};
+
+module.exports = {
+  sendEmailMessage: controllerWrapper(sendEmailMessage),
+};
--- routes/api/tasksRouter.js
+++ routes/api/tasksRouter.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const { authenticate } = require("../../middlewares");
+const sendEmailController = require("../../controllers/sendEmailController");
+const { validateBody } = require("../../middlewares");
+const { sendEmailSchema } = require("../../schemas");
 
 const router = express.Router();
 
 router.use(authenticate);
 
-router.get("/");
+router.post(
+  "/help",
+  validateBody(sendEmailSchema),
+  sendEmailController.sendEmailMessage
+);
 
 module.exports = router;

2b7b628e062a2de3dafe3bfe5db8ed6a0dacd2cb
Titotrapatonni <titovtelexfree@gmail.com>
2023-06-27 18:44:32 +0300

    fix path in taskRouter

--- routes/api/tasksRouter.js
+++ routes/api/tasksRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { authenticate } = require('../../middlewares');
+const { authenticate, validateBody } = require('../../middlewares');
 const tasksController = require('../../controllers/tasksController');
+const schemas = require('../../schemas/taskSchema');
 
 const router = express.Router();
 
 router.use(authenticate);
 
-router.get('/', tasksController.getAllTasks);
-router.post('/', tasksController.addTask);
+router.get('/', validateBody(schemas.getAllSchema), tasksController.getAllTasks);
+router.post('/', validateBody(schemas.addSchema), tasksController.addTask);
 
 module.exports = router;

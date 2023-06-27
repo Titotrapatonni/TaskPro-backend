0d424a43d121d302998c0db63778c35375ffc3a4
Titotrapatonni <titovtelexfree@gmail.com>
2023-06-27 15:23:20 +0300

    fix boardSchema

--- schemas/boardSchema.js
+++ schemas/boardSchema.js
@@ -1,12 +1,10 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 const boardSchema = Joi.object({
-    title: Joi.string()
-      .required()
-      .messages({ "any.required": "missing required Board title" }),
-    currentBg: Joi.string().uri(),
-    icon: Joi.string().uri().optional(),
-    owner: Joi.string().required(),
-  });
-  
-  module.exports = boardSchema;
+  title: Joi.string().required().messages({ 'any.required': 'missing required Board title' }),
+  currentBg: Joi.string().uri(),
+  icon: Joi.string().uri().optional(),
+  owner: Joi.string(),
+});
+
+module.exports = boardSchema;

7493ae06052a283305fbbaabebd4c9ebe04daa91
Titotrapatonni <titovtelexfree@gmail.com>
2023-06-27 17:57:26 +0300

    add task model

--- models/task.js
+++ models/task.js
@@ -1,37 +1,37 @@
-const { Schema, model } = require("mongoose");
-const { handleMongooseError } = require("../helpers");
-
+const { Schema, model } = require('mongoose');
+const { handleMongooseError } = require('../helpers');
 const taskSchema = new Schema(
   {
     title: {
       type: String,
-      default: "New Task",
+      default: 'New Task',
       required: true,
     },
     parrentColumn: {
       type: Schema.Types.ObjectId,
-      ref: "column",
+      ref: 'column',
     },
     description: {
       type: String,
-      default: "",
+      default: '',
       required: true,
     },
     priority: {
       type: String,
-      enum: ["without", "low", "medium", "high"],
-      default: "medium",
+      enum: ['without', 'low', 'medium', 'high'],
+      default: 'medium',
     },
     deadline: {
       type: String,
+      match: /^\d{2}-\d{2}-\d{4}$/,
     },
     // column: { type: Schema.Types.ObjectId, ref: "column" },
   },
   { versionKey: false }
 );
 
-taskSchema.post("save", handleMongooseError);
+taskSchema.post('save', handleMongooseError);
 
-const Task = model("task", taskSchema);
+const Task = model('task', taskSchema);
 
 module.exports = Task;
--- routes/api/tasksRouter.js
+++ routes/api/tasksRouter.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const { authenticate } = require("../../middlewares");
+const express = require('express');
+const { authenticate } = require('../../middlewares');
+const tasksController = require('../../controllers/tasksController');
 
 const router = express.Router();
 
 router.use(authenticate);
 
-router.get("/");
+router.get('/', tasksController.getAllTasks);
+router.post('/', tasksController.addTask);
 
 module.exports = router;

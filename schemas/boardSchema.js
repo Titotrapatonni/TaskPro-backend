604ce802d8a8d4a30bd398afddf71c152b7ae7a9
Titotrapatonni <titovtelexfree@gmail.com>
2023-06-28 14:39:48 +0300

    delete field owner on joi boardShema

--- schemas/boardSchema.js
+++ schemas/boardSchema.js
@@ -4,7 +4,6 @@ const boardSchema = Joi.object({
   title: Joi.string().required().messages({ 'any.required': 'missing required Board title' }),
   currentBg: Joi.number(),
   icon: Joi.string().optional(),
-  owner: Joi.string(),
 });
 
 module.exports = boardSchema;

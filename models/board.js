1bb6200789b44648aea938c4d2cff017ded24090
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-06-29 17:50:16 +0300

    fix model and schema of boards

--- models/board.js
+++ models/board.js
@@ -1,24 +1,38 @@
-const { Schema, model } = require("mongoose");
-const { handleMongooseError } = require("../helpers");
+const { Schema, model } = require('mongoose');
+const { handleMongooseError } = require('../helpers');
 
 const boardSchema = new Schema(
   {
     title: {
       type: String,
-      default: "New Board",
+      default: 'New Board',
     },
     currentBg: { type: Number, default: null },
-    icon: { type: String, default: null },
+    icon: {
+      type: String,
+      enum: [
+        'icon-hexagon-01',
+        'icon-Project',
+        'icon-lightning-02',
+        'icon-loading-03',
+        'icon-star-04',
+        'icon-puzzle-piece-02',
+        'icon-container',
+        'icon-colors',
+      ],
+      default: 'icon-hexagon-01',
+    },
     owner: {
       type: Schema.Types.ObjectId,
-      ref: "user",
+      ref: 'user',
+      required: [true, 'Owner is required'],
     },
   },
   { versionKey: false }
 );
 
-boardSchema.post("save", handleMongooseError);
+boardSchema.post('save', handleMongooseError);
 
-const Board = model("board", boardSchema);
+const Board = model('board', boardSchema);
 
 module.exports = Board;

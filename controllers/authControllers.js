05b6f0ca7bf4fb20fa56f7d0c04f36b6c597dc5d
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-06-30 17:53:26 +0300

    Added google auth  logic

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -5,7 +5,18 @@ const { HttpError } = require('../helpers');
 const { controllerWrapper } = require('../decorators');
 const cloudinary = require('cloudinary').v2;
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, FRONTEND_URL } = process.env;
+
+const googleAuth = async (req, res) => {
+  const { _id: id } = req.user;
+
+  const payload = { id };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '24h' });
+  await User.findByIdAndUpdate(id, { token });
+
+  res.redirect(`${FRONTEND_URL}/auth/register?token =${token}`);
+};
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -125,4 +136,5 @@ module.exports = {
   logout: controllerWrapper(logout),
   avatarsCloud: controllerWrapper(avatarsCloud),
   updateProfile: controllerWrapper(updateProfile),
+  googleAuth: controllerWrapper(googleAuth),
 };

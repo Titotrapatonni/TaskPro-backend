5f5ad6dc916fcab68ca98c3c45f5dbce5755a1f3
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-07-01 18:28:08 +0300

    Update boardSchema.js

--- schemas/boardSchema.js
+++ schemas/boardSchema.js
@@ -22,7 +22,7 @@ const addBoardSchema = Joi.object({
 
 const editBoardSchema = Joi.object({
   title: Joi.string().required().messages({ 'any.required': 'missing required Board title' }),
-  currentBg: Joi.string(),
+  currentBg: Joi.string().allow('').optional(),
   icon: Joi.string()
     .required()
     .valid(

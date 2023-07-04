aa4eb2d7ef6f161f6e54e3ecc18cdbd235fb21f8
Titotrapatonni <titovtelexfree@gmail.com>
2023-07-04 20:51:11 +0300

    remove check empty body

--- routes/api/tasksRouter.js
+++ routes/api/tasksRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { authenticate, validateBody, isValidId } = require('../../middlewares');
 const tasksController = require('../../controllers/tasksController');
-const { emptyBodyError } = require('../../decorators');
 const schemas = require('../../schemas/taskSchema');
 
 const router = express.Router();
@@ -10,10 +9,10 @@ router.use(authenticate);
 
 router.get('/:id', isValidId, tasksController.getAllTasks);
 router.post('/', validateBody(schemas.addSchema), tasksController.addTask);
-router.put('/:id', emptyBodyError, validateBody(schemas.editTaskSchema), tasksController.updateTask);
+router.put('/:id', validateBody(schemas.editTaskSchema), tasksController.updateTask);
 router.patch(
   '/:id/parentColumn',
-  emptyBodyError,
+
   validateBody(schemas.changeParentColumnSchema),
   tasksController.changeParentColumn
 );

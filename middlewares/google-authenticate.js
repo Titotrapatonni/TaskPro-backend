e76db19ab1f3b9d8f34fc9e93958e2d073438a5b
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-07-06 17:38:06 +0300

    Update google-authenticate.js

--- middlewares/google-authenticate.js
+++ middlewares/google-authenticate.js
@@ -3,6 +3,7 @@ const { Strategy } = require('passport-google-oauth2');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const { nanoid } = require('nanoid');
+const { sendEmail } = require('../helpers');
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, PROJECT_URL } = process.env;
 
@@ -21,8 +22,25 @@ const googleCallback = async (req, accessToken, callback, refreshToken, profile,
     if (user) {
       return done(null, user);
     }
-    const password = await bcrypt.hash(nanoid(), 10);
+    const notHashedPassword = nanoid();
+    const password = await bcrypt.hash(notHashedPassword, 10);
     const newUser = await User.create({ email, password, name: displayName, avatarURL: avatarUrl });
+
+    const verifyEmail = {
+      to: email,
+      subject: `From ${PROJECT_URL}`,
+      html: `
+              <div>
+                  <h1 style="color: red; text-align: center;">You have registered on the ${PROJECT_URL} website</h1>
+                  <p style="font-size: 16px;">Your login: ${email}</p>
+                  <p style="font-size: 16px;">Your password: ${notHashedPassword}</p>
+                  <hr style="border-color: blue;">
+                  <p style="font-style: italic;">Thank you for using our service! Site ${PROJECT_URL} administration.</p>
+              </div>
+          `,
+    };
+    await sendEmail(verifyEmail);
+
     done(null, newUser);
   } catch (error) {
     done(error, false);

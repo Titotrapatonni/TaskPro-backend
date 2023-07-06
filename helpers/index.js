aaef12208902dc28893501541783347f637448ee
Titotrapatonni <titovtelexfree@gmail.com>
2023-07-06 14:31:40 +0300

    edit helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,6 @@
-const HttpError = require("./HttpError");
-const handleMongooseError = require("./handleMongooseError");
-const sendEmail = require("./sendEmail");
+const HttpError = require('./HttpError');
+const handleMongooseError = require('./handleMongooseError');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
   HttpError,
